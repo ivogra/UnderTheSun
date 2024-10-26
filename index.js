5b1d17de7e2dd1400b9dfa9b4a1a30b380d919df
Ivo Graber <ivo.graber@ict-campus.net>
2024-10-26 21:45:01 +0200

    Der Headierende

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Happy developing ✨')
+function toggleMenu() {
+    let x = document.getElementById("navDemo");
+    let burgerIcon = document.querySelector(".burger-icon");
+    if (x.className.indexOf("w3-show") === -1) {
+        x.className += " w3-show";
+        burgerIcon.classList.add("open");
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        burgerIcon.classList.remove("open");
+    }
+}
